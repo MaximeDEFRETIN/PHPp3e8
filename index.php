7ac41659259f23b653088e55c00017c427b7beb7
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 14:32:54 +0100

    COmmit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 8 : Partie 3</title>
+  </head>
+  <body>
+    <?php
+    for ($number = 100; $number >= 0; $number--){
+    echo 'Enfin !!!!' . ' ';
+    $number -=12;
+  }
+    ?>
+  </body>
+  </body>
+</html>
